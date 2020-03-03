80b4f025de7ad5647fc6d0a4eed754a0b98a0f65
DimaMelnichuk <extezzy1995@gmail.com>
2020-03-03 10:10:06 +0300

    Add block main, info, filter
    Add adaptive gallery for block main and when hovering appears overlay with info.
    Add block info, when click on block info appears block contact.
    Add animated and sorting filter for gallery main.
    Add active menu.

--- app/js/main.js
+++ app/js/main.js
@@ -1,5 +1,28 @@
 $(function(){
  
- 	/***************** Name ********************/
+	 /***************** Info ********************/
+	 $('.contact__info').click(function(event){
+		$('.contact__list').toggleClass('active');
+	 });
+		 
+		 $(document).mouseup(function (e){ // событие клика по странице
+			if (!$(".contact__info").is(e.target) && // если клик сделан не по элементу
+				 $(".contact__info").has(e.target).length === 0) { // если клик сделан не по вложенным элементам
+				 $(".contact__list").removeClass('active'); // скрываем блок
+			}
+	  });
 
+
+	/***************** Accordion ********************/
+	$(".menu__link").click(function(event){
+	if($(".menu__link").hasClass("active")){
+		$(".menu__link").not($(this)).removeClass("active");
+	}
+	$(this).toggleClass("active");
+	});
+
+
+	/***************** mixitup ********************/
+	var mixer = mixitup('.content');
+	
 });
