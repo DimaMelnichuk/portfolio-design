373995e56da0aa643545593a27fc74264a5c32ce
DimaMelnichuk <57269449+DimaMelnichuk@users.noreply.github.com>
2020-09-26 12:39:16 +0300

    Updates

--- js/script.js
+++ js/script.js
@@ -43,6 +43,8 @@ menuBurger.addEventListener("click", function (e) {
 	menuLock.classList.toggle('lock');
 });
 
+
+
 /***************** Modal info********************/
 let info_icon = document.querySelector('.info__icon');
 let info_list = document.querySelector('.info__list');
@@ -94,8 +96,6 @@ if (document.querySelector('.wrapper')) {
 	document.querySelector('.wrapper').classList.add('loaded');
 }
 
-
-var mixer = mixitup('.content');
 document.addEventListener("DOMContentLoaded", function() {
 	var lazyloadImages;    
  
